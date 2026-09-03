3eb115f21fc781c15f9209eeb979ef25f63c8534
Fabrício Pereira <fabspereira144@gmail.com>
2026-09-03 09:57:20 -0300

    movendo código para o app recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(response):
+    return HttpResponse('HOME')
+
+def contato(response):
+    return HttpResponse('CONTATO')
+
+def sobre(response):
+    return HttpResponse('SOBRE')
