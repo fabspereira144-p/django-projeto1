d9f76048439aacc180fdc503eb8dfd07de8c9139
Fabrício Pereira <fabspereira144@gmail.com>
2026-09-18 10:29:35 -0300

    registrando recipes no admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 
 class CategotyAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
     ...
 
 
