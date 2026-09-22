70630b5e61394e67f2d6dce9a77d71bf1d79e58a
Fabrício Pereira <fabspereira144@gmail.com>
2026-09-21 21:38:25 -0300

    usando o model recipe na view home

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="recipes-home"),
-    path('recipes/<int:id>/', views.recipes, name="recipes-recipe")
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
+    path('recipes/<int:id>/', views.recipes, name="recipes-recipe"),
 ]   
